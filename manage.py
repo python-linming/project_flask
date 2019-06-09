ca19c4fbf2a9f6b3521dce062f8382f84f0fed24
Your Name <you@example.com>
2019-06-09 21:14:48 +0800

    start

---
+++ manage.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+class Config(object):
+    DEBUG = True
+
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:mysql@192.168.25.255:3306/information27'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+app = Flask(__name__)
+
+app.config.from_object(Config)
+
+db =SQLAlchemy(app)
+
+@app.route('/')
+def index():
+    return 'index333'
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    print('fuck')
